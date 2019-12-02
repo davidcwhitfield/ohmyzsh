2049bae5393c87bbb52d734d22557b3160dab565
David Curtis-Whitfield <david@venditan.com>
2019-12-02 11:42:22 +0000

    Venditan aliases

--- custom/venditan.zsh
+++ custom/venditan.zsh
@@ -1,1 +1,4 @@
+alias cdv="cd ~/Workpace"
 alias cdvc="cd ~/Workspace/venditan-commerce"
+alias vclogs="tail -f /Users/david/Workspace/venditan-commerce/docker/vc/logs/*"
+alias vcbash="/Users/david/Workspace/kubernetes-deploy/vc-bash.sh"
