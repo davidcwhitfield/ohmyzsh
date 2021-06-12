9233b453eef2f1bf1debb6cb724d84b54d9e5496
David Curtis-Whitfield <david@curtis-whitfield.co.uk>
2021-06-12 11:36:25 +0100

    Add log path

--- custom/venditan.zsh
+++ custom/venditan.zsh
@@ -1,4 +1,9 @@
-alias cdv="cd ~/Workpace"
+alias cdv="cd ~/Workspace"
+alias cdw="cd ~/Workspace"
 alias cdvc="cd ~/Workspace/venditan-commerce"
+alias slogs="cd ~/Workspace/sigmasports-web/ && docker-compose -f ~/Workspace/sigmasports-web/docker-compose.yml logs -f --tail=200"
 alias vclogs="tail -f /Users/david/Workspace/venditan-commerce/docker/vc/logs/*"
 alias vcbash="/Users/david/Workspace/kubernetes-deploy/vc-bash.sh"
+
+export ANDROID_HOME="$HOME/Library/Android/sdk"
+export PATH="$ANDROID_HOME/emulator:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$PATH"
